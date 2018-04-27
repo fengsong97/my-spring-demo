c7b8641b458ad38f32f12624c79029c8fe1ebe7c
冯松 <fengsong97@qq.com>
2018-04-27 17:32:36 +0800

    主页 hello 添加日志

--- src/main/java/com/fengsong97/spring/demo/controller/HelloController.java
+++ src/main/java/com/fengsong97/spring/demo/controller/HelloController.java
@@ -2,6 +2,8 @@ package com.fengsong97.spring.demo.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,11 +15,12 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/")
 @Api(value = "hello", description = "hello相关接口")
 public class HelloController {
+    private final static Logger logger = LoggerFactory.getLogger(HelloController.class);
 
-    
     @ApiOperation(value = "欢迎接口", notes = "")
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public String hello(@RequestParam(value = "name", required = false, defaultValue = "客人") String name) {
+        logger.info("Hello " + name);
         return "Hello " + name;
     }
 }
