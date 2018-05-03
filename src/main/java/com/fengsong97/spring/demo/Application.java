9a6d02f8fd7e5c80e259e244b1bf498f6cb322a1
冯松 <fengsong97@qq.com>
2018-05-03 09:49:12 +0800

    尝试添加 Java 配置的 bean  hibernateJpaSessionFactory

--- src/main/java/com/fengsong97/spring/demo/Application.java
+++ src/main/java/com/fengsong97/spring/demo/Application.java
@@ -2,7 +2,9 @@ package com.fengsong97.spring.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.orm.jpa.vendor.HibernateJpaSessionFactoryBean;
 
 /**
  * 创建人 fengsong
@@ -15,4 +17,13 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+
+    @Bean
+    public HibernateJpaSessionFactoryBean sessionFactory() {
+
+        return new HibernateJpaSessionFactoryBean();
+    }
+
+
 }
