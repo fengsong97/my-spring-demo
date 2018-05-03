8e39a514689056e5b6d62bafa2bc7f10d24ef290
冯松 <fengsong97@qq.com>
2018-05-03 10:26:38 +0800

    Revert "尝试添加 Java 配置的 bean  hibernateJpaSessionFactory"
    This reverts commit 9a6d02f8fd7e5c80e259e244b1bf498f6cb322a1.

--- src/main/java/com/fengsong97/spring/demo/Application.java
+++ src/main/java/com/fengsong97/spring/demo/Application.java
@@ -2,9 +2,7 @@ package com.fengsong97.spring.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.orm.jpa.vendor.HibernateJpaSessionFactoryBean;
 
 /**
  * 创建人 fengsong
@@ -17,13 +15,4 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
-
-    @Bean
-    public HibernateJpaSessionFactoryBean sessionFactory() {
-
-        return new HibernateJpaSessionFactoryBean();
-    }
-
-
 }
